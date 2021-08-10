849dbb62195402496f2627f3a49715eaba90b613
Arjun Kunjilwar <arjunkunjilwar@utexas.edu>
2021-08-09 23:47:11 -0500

    Bug fix with caching attention values

--- src/transformers/models/bart/modeling_bart.py
+++ src/transformers/models/bart/modeling_bart.py
@@ -537,7 +537,7 @@ class BartDecoderLayerSerialAttentions(nn.Module):
             residual = hidden_states
 
             # cross_attn cached key/values tuple is at positions 5,6 of present_key_value tuple
-            cross_attn_past_key_value = past_key_value[4:6] if past_key_value is not None else None
+            cross_attn_past_key_value = past_key_value[2:4] if past_key_value is not None else None
             hidden_states, r_cross_attn_weights, cross_attn_present_key_value = self.r_encoder_attn(
                 hidden_states=hidden_states,
                 key_value_states=r_encoder_hidden_states,
@@ -560,7 +560,7 @@ class BartDecoderLayerSerialAttentions(nn.Module):
             residual = hidden_states
 
             # cross_attn cached key/values tuple is at positions 3,4 of present_key_value tuple
-            cross_attn_past_key_value = past_key_value[2:4] if past_key_value is not None else None
+            cross_attn_past_key_value = past_key_value[4:6] if past_key_value is not None else None
             hidden_states, p_cross_attn_weights, cross_attn_present_key_value = self.p_encoder_attn(
                 hidden_states=hidden_states,
                 key_value_states=encoder_hidden_states,
